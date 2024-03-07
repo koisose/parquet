a053b31d98bab90d7e621cd078e4ac205fa83dfd
koisose <plokotokbelajar@gmail.com>
2024-03-07 07:17:35 +0700

    percobaan

--- index.ts
+++ index.ts
@@ -10,6 +10,7 @@ const app = express();
 // prices.parquet
 //select * from 'https://cloudflare-ipfs.com/ipfs/QmQyyT7UvX3xYfKZtfr245w5kYt63z9kK5HKi1EkjAEvVq/belanja.parquet' LIMIT 1
 //https://pochinki.fly.dev/?sql=select%20*%20from%20%27https%3A%2F%2Fcloudflare-ipfs.com%2Fipfs%2FQmQyyT7UvX3xYfKZtfr245w5kYt63z9kK5HKi1EkjAEvVq%2Fbelanja.parquet%27%20LIMIT%201
+
 app.get("/", async (req:any, res:any) => {
   try{
     const db = await Database.create(":memory:");
